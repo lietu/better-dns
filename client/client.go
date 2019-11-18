950ba2f4a0c0572ba52b392465fc2b93f22fab4e
Janne Enberg <janne.enberg@lietu.net>
2019-11-18 15:46:36 +0200

    Retry logic

--- client/client.go
+++ client/client.go
@@ -11,7 +11,7 @@ import (
 var client *dns.Client
 
 // Do a DNS query for the given request
-func Query(req *dns.Msg) *dns.Msg {
+func Query(req *dns.Msg) (res *dns.Msg) {
 	// TODO: Parallelization of multiple queries
 	// TODO: Configuration
 
@@ -24,16 +24,28 @@ func Query(req *dns.Msg) *dns.Msg {
 	port := "853"
 	server := "1.1.1.1:" + port
 
-	res, rtt, err := client.Exchange(req, server)
+	retries := 3
+	for retries > 0 {
+		retries -= 1
 
-	if err != nil {
-		go shared.ReportError(req, res, rtt)
-		log.Errorf("Caught error while querying: %#v", err)
-		if err == io.EOF && client.Net == "tcp-tls" && port != "853" {
-			log.Errorf("Maybe you're trying tcp-tls DNS to a non-TLS server? You might need to use port 853.")
+		res, rtt, err := client.Exchange(req, server)
+
+		if err != nil {
+			go shared.ReportError(req, res, rtt)
+
+			if err.Error() == "tls: DialWithDialer timed out" {
+				log.Infof("TLS connection timed out.")
+				continue
+			}
+
+			log.Errorf("Caught error while querying: %s", err)
+			if err == io.EOF && client.Net == "tcp-tls" && port != "853" {
+				log.Errorf("Maybe you're trying tcp-tls DNS to a non-TLS server? You might need to use port 853.")
+			}
+		} else {
+			go shared.ReportSuccess(req, res, rtt)
+			break
 		}
-	} else {
-		go shared.ReportSuccess(req, res, rtt)
 	}
 
 	return res
